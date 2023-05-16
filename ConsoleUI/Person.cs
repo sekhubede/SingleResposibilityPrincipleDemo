1006e761771a061b9dd92a4676802b02ea0670d3
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-05-16 09:52:43 +0200

    Added a Person class
    With two properties to be used to demo the single responsibility principle design pattern

---
+++ ConsoleUI/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LasName { get; set; }
+    }
+}
