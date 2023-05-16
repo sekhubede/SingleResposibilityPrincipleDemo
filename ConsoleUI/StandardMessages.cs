031814375f6255686a4fc6cf75b32f4927e814d7
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-05-16 10:02:13 +0200

    Added a StandardMessages class
    This class handles all the standard messages that will be displayed to the user.

---
+++ ConsoleUI/StandardMessages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class StandardMessages
+    {
+        public static void WelcomeMessage()
+        {
+            WriteLine("Welcome to my application!");
+        }
+
+        public static void EndApplication()
+        {
+            ReadLine();
+        }
+    }
+}
