f4ef97ba926c1fb522ae92020ed03125e3cf5ff5
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-05-16 10:24:02 +0200

    Added a PersonDataCapture.cs class
    This class is responsible for capturing and returning a person's information.

---
+++ ConsoleUI/PersonDataCapture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class PersonDataCapture
+    {
+        // Capture and return person information
+        public static Person Capture()
+        {
+            // Ask for user information
+            Person output = new Person();
+
+            Write("What is your first name: ");
+            output.FirstName = ReadLine();
+
+            Write("What is your last name: ");
+            output.LasName = ReadLine();
+
+            return output;
+        }
+    }
+}
