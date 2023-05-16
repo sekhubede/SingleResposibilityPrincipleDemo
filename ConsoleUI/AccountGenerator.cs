c824352b1b5987e01c7afa0e245826a369730c16
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-05-16 10:53:39 +0200

    Added an AccountGenerator.cs class
    This class handles the generation of usernames.

---
+++ ConsoleUI/AccountGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class AccountGenerator
+    {
+        public static void CeateAccount(Person user)
+        {
+            // Create a username for the person
+            WriteLine($"Your username is {user.FirstName.Substring(0, 1)}{user.LasName}");
+        }
+    }
+}
