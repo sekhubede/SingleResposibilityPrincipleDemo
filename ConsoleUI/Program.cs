545618237b857f9523789b13eddb5b84d9d65cc0
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-05-16 09:55:01 +0200

    Added logic to the Program.cs class
    The logic violates the SRP (single responsibility principle) design pattern as part of the demo to show how to implement the SRP.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace ConsoleUI
 {
@@ -6,7 +7,37 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Welcome to my application!");
+
+            // Ask for user information
+            Person user = new Person();
+
+            Write("What is your first name: ");
+            user.FirstName = ReadLine();
+
+            Write("What is your last name: ");
+            user.LasName = ReadLine();
+
+            // Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(user.FirstName))
+            {
+                WriteLine("You did not give us a valid first name!");
+                ReadLine();
+                return;
+            }
+
+            if(string.IsNullOrWhiteSpace(user.LasName))
+            {
+                WriteLine("You did not give us a valid last name!");
+                ReadLine();
+                return;
+            }
+
+            // Create a username for the person
+            WriteLine($"Your username is {user.FirstName.Substring(0, 1)}{user.LasName}");
+
+            ReadLine();
+
         }
     }
 }
