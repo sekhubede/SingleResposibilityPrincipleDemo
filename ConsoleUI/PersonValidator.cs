9c639705f83ec7cd3b0fb555154ad8181b32d03e
sekhubede <43635891+sekhubede@users.noreply.github.com>
2023-05-16 10:47:15 +0200

    Added a PersonValidator.cs class
    This checks to be sure that the first and las names of users are valid.

---
+++ ConsoleUI/PersonValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace ConsoleUI
+{
+    internal class PersonValidator
+    {
+        public static bool Validate(Person person)
+        {
+            // Checks to be sure the first and last names are valid
+            if (string.IsNullOrWhiteSpace(person.FirstName))
+            {
+                StandardMessages.DisplayValidationError("first name");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(person.LasName))
+            {
+                StandardMessages.DisplayValidationError("last name");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
